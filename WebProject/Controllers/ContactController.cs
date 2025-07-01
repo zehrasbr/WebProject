6989191b6de707bf1bfefcf8b007459ce175007d
Zehra İsbir <zehra.isbir@cyberverse.com.tr>
2025-07-01 18:27:19 +0300

    updated

--- WebProject/Controllers/ContactController.cs
+++ WebProject/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
+using System.Net;
+using WebProject.Models;
 
 namespace WebProject.Controllers
 {
@@ -8,5 +11,23 @@ namespace WebProject.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Email model)
+        {
+            MailMessage mailim = new MailMessage();
+            mailim.To.Add("zehraisbr@gmail.com");
+            mailim.From = new MailAddress("zehraisbr@gmail.com");
+            mailim.Subject = "RealRemoteLab Sayfasından Mesajınız Var. " + model.Subject;
+            mailim.Body = "Sayın yetkili, " + model.Name + " kişisinden gelen mesajın içeriği aşağıdaki gibidir. <br>" + model.Description;
+            mailim.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential("zehraisbr@gmail.com", "pqghrzgqpoqnpykk");
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            return View(mailim);
+        }
+
     }
 }
--- WebProject/Models/Email.cs
+++ WebProject/Models/Email.cs
@@ -2,6 +2,10 @@
 {
     public class Email
     {
-   
+        public int EmailId { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
     }
 }
