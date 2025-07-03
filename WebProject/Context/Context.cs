b7ec99d7475f10bdb98d96496d95ca3ac96d78c1
Zehra <zehra.isbir@cyberverse.com.tr>
2025-07-03 18:33:32 +0300

    updated

---
+++ WebProject/Context/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebProject.Models;
+
+namespace WebProject.Context
+{
+    public class Context: DbContext
+    {
+
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
